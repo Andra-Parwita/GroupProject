ad19ffccff53b48f5db3eb847df2e5167ce8845a
Andra Parwita <andraparwita@Andras-MBP.fritz.box>
2024-10-02 21:21:35 +0930

    added the main file

--- main.cpp
+++ main.cpp
@@ -1,16 +1,14 @@
 #include <SFML/Graphics.hpp> ///opt/homebrew/Cellar/sfml/2.6.1 (g++ main.cpp -I/opt/homebrew/Cellar/sfml/2.6.1/include -L/opt/homebrew/Cellar/sfml/2.6.1/lib -lsfml-graphics -lsfml-window -lsfml-system)
 
-
-int main()
-{
+int main(){
     sf::RenderWindow window(sf::VideoMode(1400, 800), "Firewall"); //
-    while (window.isOpen())
+    while (window.isOpen()) //window loop 
     {
         sf::Event event;
-        while (window.pollEvent(event))
+        while (window.pollEvent(event)) //when an event plays
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
+            if (event.type == sf::Event::Closed) //checks if the event is closed
+                window.close(); //closes window
         }
 
         window.clear(); //clears the screen
