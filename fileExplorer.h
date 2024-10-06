7e63e9cc8d8c73646b9a41fcb3809af5f7997637
Zemphis <119719357+Zemphis@users.noreply.github.com>
2024-10-06 17:28:39 +1030

    Create fileExplorer.h

---
+++ fileExplorer.h
@@ -0,0 +1,24 @@
+#ifndef FILEEXPLORER_H
+#define FILEEXPLORER_H
+
+#include "application.h"
+
+class fileExplorer : public application {
+ private:
+  int dmg;             // Amount of damage dealt
+  int attackInterval;  // Time between attacks (in seconds)
+  int elapsedTime;     // Time elapsed since the last attack
+
+ public:
+  // Constructor
+  fileExplorer(int health, int cost, int id, int dmg = 1,
+               int attackInterval = 3);
+
+  // Function to attack enemies
+  void shoot();
+
+  // Update timer for attack
+  void updateTimer(int deltaTime);
+};
+
+#endif
