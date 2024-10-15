fcb89dfe73e9b8c387c6d4b8148be2407a570c96
Andra Parwita <andraparwita@Andras-MacBook-Pro.fritz.box>
2024-10-15 22:48:14 +1030

    forgot vsCode

--- vscodeMain.cpp
+++ vscodeMain.cpp
@@ -1,16 +1,12 @@
 #include "vscode.h"
+using namespace std;
 
 int main() {
-  vscode v1(100, 15,
-            2);  // Create a vscode with health, cost, and ID
-  int gameTime = 0;
-  int deltaTime = 1;  // Each loop iteration simulates 1 second of game time
-
-  // Simulate a 10-second game loop
-  while (gameTime < 10) {
-    v1.updateTimer(deltaTime);  // Update vscode's timer
-    gameTime += deltaTime;      // Increment game time
-  }
+  vscode Obj;
+  cout << Obj.getDesc() << endl;
+  cout << "DMG: " << Obj.getDmg() << endl;
+  cout << "HEALTH: " << Obj.getHealth() << endl;
+  cout << "X: " << Obj.getAppPosition().x << " Y: " << Obj.getAppPosition().y << endl;
 
   return 0;
 }
